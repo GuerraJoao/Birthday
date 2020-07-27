7bedbdd9471544218578935b568053d1b4ca9961
GuerraJoao <68689054+GuerraJoao@users.noreply.github.com>
2020-07-27 10:22:34 +0100

    Add files via upload

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -2,18 +2,30 @@ import React, {useState, useEffect} from 'react';
 import NameInput from './NameInput';
 import CountryInput from './CountryInput';
 import {MonthName} from './helpers/MonthName';
-import {options} from './helpers/Options';
 import Table from './Table';
 import axios from 'axios';
 
+//inicialização dos arrays que guardam a lista de países (options) e os valores
+//temporários dos inputs
+
+let options = [];
+let temp = ['','','','']
+
 
 const App = () => {
+
+  //state variables que guardam a mensagem, a informação sobre todos os utilizadores (data)
+  //se há erros em algum dos inputs (errors)
   const[message, setMessage] = useState('');
   const[data, setData] = useState([]);
   const[errors, setErrors] = useState([false,false,false,false]);
-  const[temp, setTemp] = useState(['','','','']);
+
+  //variável que guarda a data e que é usada para calcular o próximo aniversário do utilizador
 
   const date = new Date();
+
+  //função que vai buscar a lista de países ao servidor. Como a lista não muda
+  //ao longo da execução do programa, só é preciso pedi-la uma vez, daí o useEffect
 
   useEffect(() => {
     axios.get('http://localhost:4000/info').then((res) => {res.data.forEach((item) => {
@@ -22,11 +34,17 @@ const App = () => {
     })
   },[])
 
+  //função que vai ser passada às componentes dos inputs para actualizar o valore
+  //da variável temp
+
   const changeValue = (prop, value) => {
-    const temp2 = temp;
-    temp2[prop] = value;
-    setTemp(temp2);
+    temp[prop] = value;
   }
+
+  //função que corre quando o utilizador clica no botão Save.
+  //Começa por verificar se os campos estão preenchidos (função validate)
+  //e depois verifica se a data de nascimento é válida (função validateBirthday)
+  //Se não houver erros, actualiza a mensagem e guarda os dados que estão na variável temp na variável data
 
   const onClick = () => {
     temp.forEach((item, i) => {
@@ -46,11 +64,12 @@ const App = () => {
 
     if(!isError){
       setMessage(`Hello, ${temp[0]} ${temp[1]} from ${temp[2]}. On ${day} of ${MonthName(month)} of ${nextBirthday(day,month)} you will be ${nextBirthday(day,month) - year} years old!`);
-      setData([...data, {name: `${temp[0]} ${temp[1]}`, country: temp[2], birthday: temp[3] }]);
+      setData([...data, {name: `${temp[0]} ${temp[1]}`, country: temp[2], birthday: `${month}/${day}/${year}` }]);
     }
     else{ setData([...data]);}
-    console.log(data)
   }
+
+  //definição das helper functions
 
   const validate = (index) => {
     const temp2 = errors;
@@ -65,10 +84,9 @@ const App = () => {
   }
 
   const validateBirthday = (day, month, year) => {
-    if(!Number(day) || !Number(month) || !Number(year) || month > 12 || month < 0 || day < 0 || day > 31 || !(year < date.getFullYear() || (year === date.getFullYear() && month <= date.getMonth() + 1 && day < date.getDate()))){
+    if(!Number(day) || !Number(month) || !Number(year) || month > 12 || month < 0 || day < 0 || day > 31 || (['04','06','09','11'].indexOf(month) >= 0 && day > 30) || (month == '02' && day > (28 + (year%4 == 0))) || !(year < date.getFullYear() || (year === date.getFullYear() && month <= date.getMonth() + 1 && day < date.getDate()))){
       let temp2 = errors;
       temp2[3] = true;
-      console.log(temp2)
         setErrors(temp2);
     }
   }
@@ -84,7 +102,7 @@ const App = () => {
     <div className="ui container" >
       <div className="ui grid" style={{paddingTop: "10px"}}>
         <div className="eight wide column">
-          <NameInput label="Name" changeValue={changeValue} prop={0} isError={errors[0]}/>
+          <NameInput label="Name" changeValue={changeValue} prop={0} isError={errors[0]} />
           <NameInput label="Surname" changeValue={changeValue} prop={1} isError={errors[1]}/>
           <CountryInput label="Country" options={options} changeValue={changeValue} prop={2} isError={errors[2]}/>
           <NameInput label="Birthday" changeValue={changeValue} prop={3} isError={errors[3]}/>
